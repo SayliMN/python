466e47c6f0a46cd0ac9b4b62f57cd6d967d7b29b
SayliMN <saylinarkhede@yahoo.com>
2025-07-04 19:26:57 +0530

    overridding

--- method-overloading.py
+++ method-overloading.py
@@ -1,30 +1,42 @@
 class Student:
-    def __init__(self, m1, m2):
+    def __init__(self, m1, m2, m3):
         self.m1 = m1
         self.m2 = m2
+        self.m3 = m3
 
     def __add__(self, other):
         m1 = self.m1 + other.m1
-        m2 = self.m2 + self.m2
+        m2 = self.m2 + other.m2
+        m3 = self.m3 + other.m3
 
-        s3 = Student(m1, m2)
+        s3 = Student(m1, m2, m3)
         return s3
 
     def __gt__(self, other):
-        r1 = self.m1 + self.m2
-        r2 = other.m1 + other.m2
+        r1 = self.m1 + self.m2 + self.m3
+        r2 = other.m1 + other.m2 + other.m3
 
         if r1 > r2:
             return True
         else:
             return False
+    def show(self):
+        print("Hello there!")
+
+class B(Student):
+    def show(self):
+        print("Hello there TWICE!!")
 
 
-s1 = Student(50, 34)
-s2 = Student(97, 99)
+b = B(50, 34, 43)
+b.show()
+
+s1 = Student(50, 34, 43)
+s2 = Student(97, 99, 96)
+# s1.show()
 
 s3 = s1 + s2
-print(s3.m1)
+print(s3.m3)
 
 if s1 > s2:
     print("s1 wins")
