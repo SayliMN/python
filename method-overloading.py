780bb1c6de21ef4e1210f31da9b2265a3f262cf7
SayliMN <saylinarkhede@yahoo.com>
2025-07-04 19:43:30 +0530

    example

--- method-overloading.py
+++ method-overloading.py
@@ -12,6 +12,14 @@ class Student:
         s3 = Student(m1, m2, m3)
         return s3
 
+    def __sub__(self, other):
+        m1 = self.m1 - other.m1
+        m2 = self.m2 - other.m2
+        m3 = self.m3 - other.m3
+
+        s3 = Student(m1, m2, m3)
+        return s3
+
     def __gt__(self, other):
         r1 = self.m1 + self.m2 + self.m3
         r2 = other.m1 + other.m2 + other.m3
@@ -34,6 +42,7 @@ class Student:
 
         return s
 
+
 class B(Student):
     def show(self):
         print("Hello there TWICE!!")
@@ -44,13 +53,17 @@ b.show()
 
 s1 = Student(50, 34, 43)
 s2 = Student(97, 99, 96)
+s1.show()
+
 ss = Student(50,100, 99)
-# s1.show()
 print(ss.sum(15,500))
 
 
 s3 = s1 + s2
 print(s3.m3)
+
+sb = s1 - s2
+print(sb.m3)
 
 if s1 > s2:
     print("s1 wins")
